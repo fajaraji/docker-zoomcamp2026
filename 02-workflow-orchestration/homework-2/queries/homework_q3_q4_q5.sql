75a8967658f8adaa7b5327f0e53fe2afd01fcbc9
Fajar Aji Pamungkas <45536649+fajaraji@users.noreply.github.com>
2026-02-04 04:32:09 +0000

    Revised SQL queries

--- 02-workflow-orchestration/homework-2/queries/homework_q3_q4_q5.sql
+++ 02-workflow-orchestration/homework-2/queries/homework_q3_q4_q5.sql
@@ -1,14 +1,14 @@
 -- Question 3 (Yellow Taxi 2020)
 SELECT COUNT(*) 
-FROM `project_id_kamu.dataset_kamu.yellow_tripdata`
+FROM `de-zoomcamp-2026-01.zoomcamp.yellow_tripdata`
 WHERE filename LIKE '%2020%';
 
 -- Question 4 (Green Taxi 2020)
 SELECT COUNT(*) 
-FROM `project_id_kamu.dataset_kamu.green_tripdata`
+FROM `de-zoomcamp-2026-01.zoomcamp.green_tripdata`
 WHERE filename LIKE '%2020%';
 
 -- Question 5 (Yellow Taxi Maret 2021)
 SELECT COUNT(*) 
-FROM `project_id_kamu.dataset_kamu.yellow_tripdata`
+FROM `de-zoomcamp-2026-01.zoomcamp.yellow_tripdata`
 WHERE filename LIKE '%2021-03%';
